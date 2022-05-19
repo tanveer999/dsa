719f9fa650252c7f2a4a562cb2548d05f2b1871a
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-05-19 17:47:35 +0530

    update: remove duplicates from sorted array

--- gfg/Array/RemoveDuplicatesFromSortedArray.java
+++ gfg/Array/RemoveDuplicatesFromSortedArray.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 public class RemoveDuplicatesFromSortedArray {
     public static void main(String[] args) {
         // int[] arr = {1, 2, 3, 4, 4, 5};
-        // int[] arr = {1, 2, 3, 4, 4, 4, 5, 6, 6, 6, 7};
+        int[] arr = {1, 2, 3, 4, 4, 4, 5, 6, 6, 6, 7};
         // int[] arr = {1};
-        int[] arr = {};
+        // int[] arr = {};
+        // int[] arr = {10, 10, 10};
         int n = arr.length;
         System.out.println("Original array: " + Arrays.toString(arr));
         // removeDuplicate1(arr, n);
+
+        System.out.print("Array after removing duplicate elements: ");
+        int size = removeDuplicate2(arr, n);
+        for(int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
     }
 
@@ -34,10 +42,23 @@ public class RemoveDuplicatesFromSortedArray {
         if(n > 1)
             newArr[++count] = arr[--i]; // for last element
 
-        System.out.print("Array after removing duplicates: " );
+        System.out.print("Array after removing duplicate elements: " );
         for(i = 0; i < newLength; i++) {
             System.out.print(newArr[i] + " ");
         }
         System.out.println();
+    }
+
+    // auxillary space O(1)
+    public static int removeDuplicate2(int[] arr, int n) {
+        int i = 1;
+
+        for(int j = 1; j < n; j++) {
+            if(arr[j] != arr[i - 1]) {
+                arr[i] = arr[j];
+                i++;
+            }
+        }
+        return i;
     }
 }
