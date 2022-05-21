7ffe021d519dadd6d029504ecfb7aaee2a341489
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-05-21 17:55:30 +0530

    update: leaders in array

--- gfg/Array/LeadersInAnArray.java
+++ gfg/Array/LeadersInAnArray.java
@@ -2,14 +2,15 @@ package Array;
 
 public class LeadersInAnArray {
     public static void main(String[] args) {
-        // int[] arr = {7, 10, 4, 3, 6, 5, 2};
+        int[] arr = {7, 10, 4, 3, 6, 5, 2};
         // int[] arr = {10, 20, 30};
-        int[] arr = {30, 20, 10};
+        // int[] arr = {30, 20, 10};
         int n = arr.length;
-        printLeaders(arr, n);
+        // printLeaders1(arr, n);
+        printLeaders2(arr, n);
     }
 
-    public static void printLeaders(int[] arr, int n) {
+    public static void printLeaders1(int[] arr, int n) {
         for(int i = 0; i < n; i++) {
             boolean flag = true;
 
@@ -29,5 +30,22 @@ public class LeadersInAnArray {
                 System.out.print(arr[i] + " ");
             }
         }
+    }
+
+    public static void printLeaders2(int[] arr, int n) {
+        int[] temp = new int[n];
+        int count = 0;
+        int max = arr[n - 1];
+        temp[count++] = max;
+        for(int i = n - 2; i >= 0; i--) {
+            if(arr[i] > max) {
+                temp[count++] = arr[i];
+                max = arr[i];
+            }
+        }
+        for(int i = count - 1; i >= 0; i--) {
+            System.out.print(temp[i] + " ");
+        }
+        System.out.println();
     }
 }
