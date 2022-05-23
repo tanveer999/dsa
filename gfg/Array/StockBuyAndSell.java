ec0962439d87c3c489b0b54ed6ea36015bc9faee
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-05-23 14:34:03 +0530

    update: stock buy and sell

--- gfg/Array/StockBuyAndSell.java
+++ gfg/Array/StockBuyAndSell.java
@@ -4,13 +4,14 @@ public class StockBuyAndSell {
     public static void main(String[] args) {
         // int[] arr = {1, 5, 3, 1, 2, 8};
         // int[] arr =  {1, 5, 3, 1, 2, 8, 1};
-        int[] arr = {1, 5, 3, 8, 12}; 
+        // int[] arr = {1, 5, 3, 8, 12}; 
         // int[] arr = {30, 20, 10};
-        // int[] arr = {10, 20, 30};
-        System.out.println(buyAndSell(arr, arr.length));
+        int[] arr = {10, 20, 30};
+        System.out.println(buyAndSell1(arr, arr.length));
+        System.out.println(buyAndSell2(arr, arr.length));
     }
 
-    public static int buyAndSell(int[] arr, int n) {
+    public static int buyAndSell1(int[] arr, int n) {
         int sum = 0, max = 0, min = Integer.MAX_VALUE;
 
         for(int i = 0; i < n; i++) {
@@ -33,6 +34,26 @@ public class StockBuyAndSell {
             }
         }
         // if(max > 0) sum += (max - min);
+        return sum;
+    }
+
+    // time complexity O(n)
+    public static int buyAndSell2(int[] arr, int n) {
+        int sum = 0, max = 0, min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] < min && max == 0) {
+                min = arr[i];
+                continue;
+            }
+            if(arr[i] > max) max = arr[i];
+            if (max != 0 && arr[i] < max) {
+                sum += (max - min);
+                max = 0;
+                min = arr[i];
+            }
+        }
+        if(max > 0) sum += (max - min);
         return sum;
     }
 }
