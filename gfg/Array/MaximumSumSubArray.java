ed73af98ebf40cb20de782afd7f6931add510c22
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-05-25 10:43:34 +0530

    update: maximum sum subarray

--- gfg/Array/MaximumSumSubArray.java
+++ gfg/Array/MaximumSumSubArray.java
@@ -4,9 +4,11 @@ public class MaximumSumSubArray {
     public static void main(String[] args) {
         // int[] arr = {1, 2, 3};
         // int[] arr = {2, 3, -8, 7, -1, 2, 3};
-        // int[] arr = {5, 8, 3};
-        int[] arr = {-6, -1, -8};
+        int[] arr = {5, 8, 3};
+        // int[] arr = {-6, -1, -8};
+        // int[] arr = {-3, 8, -2, 4, -5, 6};
         System.out.println(maximumSum1(arr, arr.length));
+        System.out.println(maximumSum2(arr, arr.length));
     }
 
     public static int maximumSum1(int[] arr, int n) {
@@ -21,5 +23,15 @@ public class MaximumSumSubArray {
             }
         }
         return max;
+    }
+
+    public static int maximumSum2(int[] arr, int n) {
+        int sum = arr[0], max = arr[0];
+
+        for(int i = 1; i < n; i++) {
+            max = Math.max(max + arr[i], arr[i]);
+            sum = Math.max(sum, max);
+        }
+        return sum;
     }
 }
