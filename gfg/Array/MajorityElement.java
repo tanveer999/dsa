182da09e41d31d66f26461bd50db0f6580b2c379
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-05-26 19:07:37 +0530

    update: majority element

--- gfg/Array/MajorityElement.java
+++ gfg/Array/MajorityElement.java
@@ -5,6 +5,7 @@ public class MajorityElement {
         // int[] arr = {8, 3, 4, 8, 8};
         int[] arr = {3, 7, 4, 7, 7, 5};
         majorityElement1(arr, arr.length);
+        majorityElement2(arr, arr.length);
     }
 
     public static void majorityElement1(int[] arr, int n) {
@@ -29,6 +30,35 @@ public class MajorityElement {
                 }
             }
             System.out.println();
+        } else {
+            System.out.println(-1);
+        }
+    }
+
+    // O(n) 
+    // boyer-moore voting algo
+    public static void majorityElement2(int[] arr, int n) {
+        int count = 1;
+        int res = 0;
+
+        for(int i = 1; i < n; i++) {
+            if(arr[res] == arr[i]) {
+                count++;
+            } else {
+                count--;
+            }
+            if(count  == 0) {
+                res = i;
+                count = 1;
+            }
+        }
+
+        count = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == arr[res]) count++;
+        }
+        if(count > n / 2) {
+            System.out.println(res);
         } else {
             System.out.println(-1);
         }
--- gfg/todo.md
+++ gfg/todo.md
@@ -2,4 +2,4 @@ Array
 1. trapping rain water
 2. maximum sum subarray
 3. maximum circular sub array sum
-4. check majority element
+4. check majority element (boyer-moore voting algo)
