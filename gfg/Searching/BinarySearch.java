09c8fdc7e101a86fd5d94bb8be4a9b6990ccc9ab
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-05-31 11:41:03 +0530

    update: binary search

--- gfg/Searching/BinarySearch.java
+++ gfg/Searching/BinarySearch.java
@@ -3,8 +3,9 @@ package Searching;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {1, 3, 5, 6, 7, 8, 9};
-        int element = 20;
+        int element = 9;
         System.out.println(binarySearch(arr, arr.length, element));
+        System.out.println(binarySearchRecursive(arr, arr.length, 0, arr.length - 1, element));
     }
 
     public static int binarySearch(int[] arr, int n, int element) {
@@ -24,5 +25,19 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int binarySearchRecursive(int[] arr, int n, int start, int end, int element) {
+        if (start > end) {
+            return -1;
+        }
+
+        int mid = (start + end) / 2;
+        if(arr[mid] == element) {
+            return mid;
+        } else if(element < arr[mid]) {
+            return binarySearchRecursive(arr, n, start, mid - 1, element);
+        } else {
+            return binarySearchRecursive(arr, n, mid + 1, end, element);
+        }
+    }
 
 }
