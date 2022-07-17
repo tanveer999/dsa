f4c2cd4e7a2ca4d0c1bcd109483a8185fa9c90bf
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-07-17 20:44:32 +0530

    add: heapify and extract

--- gfg/Heap/HeapImplementation.java
+++ gfg/Heap/HeapImplementation.java
@@ -29,13 +29,54 @@ class Heap {
 public class HeapImplementation {
     public static void main(String[] args) {
         Heap heap = new Heap(10);
-        int[] temp = {10, 20, 15, 40, 50, 100, 25, 45};
+        int[] temp = {40, 20, 30, 35, 25, 80, 32, 100, 70, 60};
         for(int i = 0; i < temp.length; ++i) {
             heap.arr[i] = temp[i];
             heap.size++;
         }
-        int item = 12;
-        insert(heap, item);   
+        // int item = 12;
+        // insert(heap, item);   
+        minHeapify(heap, 0);
+        System.out.println(Arrays.toString(heap.arr));
+    }
+
+    static void swap(int[] arr, int x, int y) {
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    static int extractMin(Heap heap) {
+        if(heap.size == 0) {
+            return Integer.MIN_VALUE;
+        }
+
+        if(heap.size == 1) {
+            heap.size--;
+            return heap.arr[0];
+        }
+
+        swap(heap.arr, 0, heap.size - 1);
+        heap.size--;
+        minHeapify(heap, 0);
+        return heap.arr[heap.size];
+    }
+
+    public static void minHeapify(Heap heap, int index){ 
+        int lt = heap.left(index), rt = heap.right(index);
+        int smallest = index;
+
+        if(lt < heap.size && heap.arr[lt] < heap.arr[smallest]) {
+            smallest = lt;
+        }
+        if(rt < heap.size && heap.arr[rt] < heap.arr[smallest]) {
+            smallest = rt;
+        }
+
+        if(smallest != index){
+            swap(heap.arr, smallest, index);
+            minHeapify(heap, smallest);
+        }
     }
 
     public static void insert(Heap heap, int item) {
@@ -53,10 +94,8 @@ public class HeapImplementation {
         System.out.println(Arrays.toString(heap.arr));
     }
 
-    static void swap(int[] arr, int x, int y) {
-        int temp = arr[x];
-        arr[x] = arr[y];
-        arr[y] = temp;
-    }
+
+
+    
 
 }
