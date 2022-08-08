79a11cfbf841407c2a7930d54a302a288d9c343a
Tanveer Ahmed <tanveerahmed2619@gmail.com>
2022-08-08 20:32:47 +0530

    add: equal sum partition

--- gfg/DynamicProgramming/EqualSumPartition.java
+++ gfg/DynamicProgramming/EqualSumPartition.java
@@ -28,7 +28,23 @@ public class EqualSumPartition {
     }
 
     static boolean subsetSum(int[] arr, int sum) {
-        return true;
+        int n = arr.length;
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = true;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j<= sum; j++) {
+                if(arr[i - 1] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - arr[i - 1]];
+                }
+            }
+        }
+        return dp[n][sum];
     }
 
 
